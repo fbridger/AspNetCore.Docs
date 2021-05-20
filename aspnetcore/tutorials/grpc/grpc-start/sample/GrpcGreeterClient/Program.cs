ad6f0b0cc74628de5222033f73c4ec2b071f2375
Federico Bridger <fbridger@gmail.com>
2021-05-20 20:15:02 -0300

    Added tests

--- aspnetcore/tutorials/grpc/grpc-start/sample/GrpcGreeterClient/Program.cs
+++ aspnetcore/tutorials/grpc/grpc-start/sample/GrpcGreeterClient/Program.cs
@@ -10,7 +10,10 @@ namespace GrpcGreeterClient
 {
     class Program
     {
-        public const int TaskCount = 500;
+        private const int TotalRequests = 100;
+        private const bool LogDetails = false;
+        private static ParallelOptions parallelOptions = new ParallelOptions() { MaxDegreeOfParallelism = 50 };
+
         #region snippet
         static void Main(string[] args)
         {
@@ -20,25 +23,94 @@ namespace GrpcGreeterClient
             var reply = client.SayHello(new HelloRequest { Name = "GreeterClient" });
             Console.WriteLine($"{DateTime.Now} - Result {reply}");
 
-            var pingTasks = new List<Task>();
-            Console.WriteLine($"{DateTime.Now} - Starting {TaskCount} tasks");
-            var stopwatch = Stopwatch.StartNew();
-
-            var parallelOptions = new ParallelOptions() { MaxDegreeOfParallelism = 50 };
-            Parallel.For(0, TaskCount, parallelOptions, (i) =>
-            {
-                Console.WriteLine($"{DateTime.Now} - Start task {i}");
-                var reply = client.SayHello(new HelloRequest { Name = "GreeterClient" });
-                Console.WriteLine($"{DateTime.Now} - End task {i}: {reply}");
-            });
-            stopwatch.Stop();
-
-            Console.WriteLine($"{DateTime.Now} - {TaskCount} tasks took: {stopwatch.ElapsedMilliseconds}ms");
-
+            Test();
+            TestReusingGrpcChannel();
+            TestConcurrentTasks();
+            TestConcurrentTasksReusingGrpcChannel();
+            
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
+
+        static void Test()
+        {
+            Console.WriteLine($"{DateTime.Now} - Starting {nameof(Test)} {TotalRequests} tasks");
+
+            var stopwatch = Stopwatch.StartNew();
+            for (int i = 0; i < TotalRequests; i++)
+            {
+                if (LogDetails) Console.WriteLine($"{DateTime.Now} - Start task {i}");
+                using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+                var client = new Greeter.GreeterClient(channel);
+                var reply = client.SayHello(new HelloRequest { Name = "GreeterClient" });
+                if (LogDetails) Console.WriteLine($"{DateTime.Now} - End task {i}: {reply}");
+
+            }
+            stopwatch.Stop();
+
+            Console.WriteLine($"{DateTime.Now} - {nameof(Test)} for {TotalRequests} tasks took: {stopwatch.ElapsedMilliseconds}ms");
+
+        }
+
+        static void TestReusingGrpcChannel()
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            Console.WriteLine($"{DateTime.Now} - Starting {nameof(TestReusingGrpcChannel)} {TotalRequests} tasks");
+
+            var stopwatch = Stopwatch.StartNew();
+            for (int i = 0; i < TotalRequests; i++)
+            {
+                if (LogDetails) Console.WriteLine($"{DateTime.Now} - Start task {i}");
+                var reply = client.SayHello(new HelloRequest { Name = "GreeterClient" });
+                if (LogDetails) Console.WriteLine($"{DateTime.Now} - End task {i}: {reply}");
+
+            }
+            stopwatch.Stop();
+
+            Console.WriteLine($"{DateTime.Now} - {nameof(TestReusingGrpcChannel)} for {TotalRequests} tasks took: {stopwatch.ElapsedMilliseconds}ms");
+
+        }
+
+        static void TestConcurrentTasks()
+        {
+            Console.WriteLine($"{DateTime.Now} - Starting {nameof(TestConcurrentTasks)} {TotalRequests} tasks");
+
+            var stopwatch = Stopwatch.StartNew();
+            Parallel.For(0, TotalRequests, parallelOptions, (i) =>
+            {
+                if (LogDetails) Console.WriteLine($"{DateTime.Now} - Start task {i}");
+                using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+                var client = new Greeter.GreeterClient(channel);
+                var reply = client.SayHello(new HelloRequest { Name = "GreeterClient" });
+                if (LogDetails) Console.WriteLine($"{DateTime.Now} - End task {i}: {reply}");
+            });
+            stopwatch.Stop();
+
+            Console.WriteLine($"{DateTime.Now} - {nameof(TestConcurrentTasks)} for {TotalRequests} tasks took: {stopwatch.ElapsedMilliseconds}ms");
+
+        }
+
+        static void TestConcurrentTasksReusingGrpcChannel()
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            Console.WriteLine($"{DateTime.Now} - Starting {nameof(TestConcurrentTasksReusingGrpcChannel)} {TotalRequests} tasks");
+            var stopwatch = Stopwatch.StartNew();
+            Parallel.For(0, TotalRequests, parallelOptions, (i) =>
+            {
+                if (LogDetails) Console.WriteLine($"{DateTime.Now} - Start task {i}");
+                var reply = client.SayHello(new HelloRequest { Name = "GreeterClient" });
+                if (LogDetails) Console.WriteLine($"{DateTime.Now} - End task {i}: {reply}");
+            });
+            stopwatch.Stop();
+
+            Console.WriteLine($"{DateTime.Now} - {nameof(TestConcurrentTasksReusingGrpcChannel)} for {TotalRequests} tasks took: {stopwatch.ElapsedMilliseconds}ms");
+
+        }
+
         #endregion
     }
 }
