c23da363f3633b78acc3bbc999b5587bdeccdedc
Federico Bridger <fbridger@gmail.com>
2021-05-20 19:57:03 -0300

    Adding multiple concurrent tasks test to example

--- aspnetcore/tutorials/grpc/grpc-start/sample/GrpcGreeterClient/Program.cs
+++ aspnetcore/tutorials/grpc/grpc-start/sample/GrpcGreeterClient/Program.cs
@@ -1,5 +1,6 @@
 ﻿#region snippet2
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace GrpcGreeterClient
 {
     class Program
     {
-        public const int TaskCount = 50;
+        public const int TaskCount = 500;
         #region snippet
         static void Main(string[] args)
         {
@@ -19,24 +20,20 @@ namespace GrpcGreeterClient
             var reply = client.SayHello(new HelloRequest { Name = "GreeterClient" });
             Console.WriteLine($"{DateTime.Now} - Result {reply}");
 
-            var pingTasks = new Task[TaskCount];
-            Console.WriteLine($"{DateTime.Now} - Starting {pingTasks.Length} tasks");
+            var pingTasks = new List<Task>();
+            Console.WriteLine($"{DateTime.Now} - Starting {TaskCount} tasks");
             var stopwatch = Stopwatch.StartNew();
-            for (var i = 0; i < pingTasks.Length; i++)
+
+            var parallelOptions = new ParallelOptions() { MaxDegreeOfParallelism = 50 };
+            Parallel.For(0, TaskCount, parallelOptions, (i) =>
             {
-                pingTasks[i] = Task.Factory.StartNew(() =>
-                {
-                    Console.WriteLine($"{DateTime.Now} - Start task {i}");
-                    var client = new Greeter.GreeterClient(channel);
-                    var reply = client.SayHello(new HelloRequest { Name = "GreeterClient" });
-                    Console.WriteLine($"{DateTime.Now} - Result task {i}: {reply}");
-                });
-            }
-            Console.WriteLine("Waiting for tasks to complete");
-            Task.WaitAll(pingTasks);
+                Console.WriteLine($"{DateTime.Now} - Start task {i}");
+                var reply = client.SayHello(new HelloRequest { Name = "GreeterClient" });
+                Console.WriteLine($"{DateTime.Now} - End task {i}: {reply}");
+            });
             stopwatch.Stop();
 
-            Console.WriteLine($"{DateTime.Now} - {pingTasks.Length} tasks took: {stopwatch.ElapsedMilliseconds}ms");
+            Console.WriteLine($"{DateTime.Now} - {TaskCount} tasks took: {stopwatch.ElapsedMilliseconds}ms");
 
 
             Console.WriteLine("Press any key to exit...");
